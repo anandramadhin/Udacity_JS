932e239b4a54783979beef506f81cb90441cd647
Anand Ramadhin <anandramadhin1@gmail.com>
2016-04-28 16:26:43 -0400

    Resume Builder init.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = HTMLheaderName.replace("%data%", "Anand Ramadhin");
+var role = HTMLheaderRole.replace("%data%", "WEB DEVELOPER");
+
+var skills = ["HTML5 ", "CSS3 ", "JS "];
+
+var bio = {
+	"name" : HTMLheaderName.replace("%data%", "Anand Ramadhin"),
+	"role" : HTMLheaderRole.replace("%data%", "WEB DEVELOPER"),
+	"contact_info" : 
+	{
+		"mobile" : "(868) 460-6839",
+		"email" : "anandramadhin1@gmail.com",
+		
+	},
+	"pic_url" : "images/me.jpg",
+	"welcome" : "Welcome to my resume",
+	"skills" : skills
+};
+
+$("#header").prepend(role);
+$("#header").prepend(name);
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+//$("#main").prepend(bio.contact_info);
+
